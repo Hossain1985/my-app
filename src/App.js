21a46bc1b945fc3124ae5e076697d86251a42ee3
Hossain1985 <shikhor31@me.com>
2022-08-09 01:26:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Card from './components/Card';
+
+function App() {
+  return  <div>
+  <h1 className='headingStyle'>MucktadirHossain</h1>
+   <Card />
+   <Card />
+   <Card />
+  </div>
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+const fDev = "frllStakeDevopler"
+
+function Card(){
+  return    <div className='card'>
+             <h2 className='fstake'>{fDev}</h2>
+            </div>
+ 
+  
+}
+export default Card;
